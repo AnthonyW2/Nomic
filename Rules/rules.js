6fa48c7b29f3d3402bf26ddcde4ff847f2d7fe86
Anthony Wilson <awilsonprogramming@gmail.com>
2022-05-21 14:14:50 +1000

    Synced changes

--- Rules/rules.js
+++ Rules/rules.js
@@ -36,34 +36,6 @@ var DisplayRules = () => {
   
   rulesListElement.appendChild(ruleTree);
   
-  ///for(var r = 0;r < rules.length;r ++){
-  ///  
-  ///  //Add the rule content to the main list
-  ///  var ruleElement = document.createElement("div");
-  ///  
-  ///  ruleElement.innerHTML = r+": "+rules[r].content+"<br>";
-  ///  ruleElement.classList.add("rule");
-  ///  ruleElement.id = "rule-"+r;
-  ///  
-  ///  rulesListElement.appendChild(ruleElement);
-  ///  
-  ///  
-  ///  //Add the rule to the summary table
-  ///  var ruleTableRow = document.createElement("tr");
-  ///  
-  ///  ruleTableRow.innerHTML = "<td>"+r+"</td>";
-  ///  ruleTableRow.innerHTML += "<td>"+rules[r].summary+"</td>";
-  ///  
-  ///  var taglist = [];
-  ///  for(var t = 0;t < rules[r].tags.length;t ++){
-  ///    taglist.push("<div class=\"rule-tag\">"+rules[r].tags[t]+"</div>");
-  ///  }
-  ///  ruleTableRow.innerHTML += "<td>"+taglist.join(",")+"</td>";
-  ///  
-  ///  ruleSummaryTableElement.appendChild(ruleTableRow);
-  ///  
-  ///}
-  
 }
 
 //Recursive function to create a HTML representation of a rule and its subrules
@@ -71,7 +43,7 @@ var createRuleBranch = (rule) => {
   
   var ruleElement = document.createElement("div");
   
-  ruleElement.innerHTML = rule.getIDPath()+" - "+rule.summary+"<br>"+rule.content;
+  ruleElement.innerHTML = rule.getIDPath()+" - "+rule.summary+"<br>"+rule.content.replaceAll("\n","<br>\n");
   ruleElement.classList.add("rule");
   ruleElement.id = "rule-"+rule.id;
   
